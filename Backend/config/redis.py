3ddb6f279c62a7a6d4104c9a7e368039f64f1df2
Ghazi <ghazihussainsyed721@gmail.com>
2026-02-23 22:13:54 -0600

    changed req for deployment

--- Backend/config/redis.py
+++ Backend/config/redis.py
@@ -1,4 +1,4 @@
 from redis.asyncio import Redis
 import os
 
-redis = Redis.from_url(os.getenv("REDIS_URL", "redis://localhost:6379"), decode_responses=True)
+redis = Redis.from_url(os.getenv("REDIS_URL", "redis://redis:6379"), decode_responses=True)
