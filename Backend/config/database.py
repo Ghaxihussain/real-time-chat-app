b54cf35d3dea36ae48ad484381f082792085e59f
Ghazi <ghazihussainsyed721@gmail.com>
2026-02-24 22:57:00 -0600

    Fixed Duplicated Users

--- Backend/config/database.py
+++ Backend/config/database.py
@@ -30,7 +30,7 @@ class Message(Base):
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
-    username = Column(String, nullable=False)
+    username = Column(String, nullable=False, unique = True)
     name = Column(String, nullable=False)
     password = Column(String, nullable=False)
     is_deleted = Column(Boolean, default=False)
